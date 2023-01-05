ca4f3ab77ec40f895ba50650a75fd09bb488d99b
= <=>
2023-01-05 09:30:12 -0300

    setup

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import { Heading, Text } from '@ignite-ui/react'
+export default function Home() {
+  return (
+    <div>
+      <Heading>Hello world</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis incidunt
+        sed cupiditate aperiam neque. Illo esse doloribus quod quos amet
+        assumenda exercitationem, facere quas, voluptatem impedit ad
+        necessitatibus explicabo sapiente?
+      </Text>
+    </div>
+  )
+}
